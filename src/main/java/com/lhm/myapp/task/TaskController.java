84ef2ce7b5edec03371d6a8ca3882b16ee3bfe50
LeeKiara <hbin1028@gmail.com>
2023-11-23 23:43:50 +0900

    cloud 적용에 따른 도메인 변경

--- src/main/java/com/lhm/myapp/task/TaskController.java
+++ src/main/java/com/lhm/myapp/task/TaskController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.*;
 
 @RestController
-//@RequestMapping("/project/tasks")
+@RequestMapping("/api")
 public class TaskController {
 
     @Autowired
